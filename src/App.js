77efaf00a283ef7196a9ec9713f968df9b1e7e9c
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-01-06 14:46:14 +0100

    Adventure

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+
+import "./index.css";
+
+//import Footer from "./Footer";
+//import Page from "./Page";
+import Loading from "./Loading";
+import Tours from "./Tours";
+import Tour from "./Tour";
+const url = "https://course-api.com/react-tours-project";
+
+function App() {
+  const [loading, setLoading] = useState(false);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchTours();
+  }, []);
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  return (
+    <main>
+      <Tours tours={tours} />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/Tour.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+
+const Tour = ({ id, image, info, price }) => {
+  return <h2> Single tour component</h2>;
+};
+
+export default Tour;
